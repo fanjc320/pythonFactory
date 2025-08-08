3faf68b07568ab93edd51572cb7243483a441058
jackyfan <894020798@qq.com>
2025-08-08 13:41:18 +0800

    split polygon ok basically

--- common/MyPolygon1.py
+++ common/MyPolygon1.py
@@ -10,11 +10,13 @@
 # line 19, AttributeError: 'dict' object has no attribute'vertices'
 import math
 
-
+sPolyKey = "poly_origin"
 class Vertex:
-    def __init__(self, x, y, index=None):
+    def __init__(self, x, y, index=None, index1=0, index2=0):
         self.x = x
         self.y = y
+        self.index1 = index1 # 截断产生的点，对应的线段端点的坐标
+        self.index2 = index2
         self.index = index if index is not None else -1  # -1 for new vertices
 
     def distance_to(self, other):
@@ -56,11 +58,30 @@ class PolygonProcessor:
         for i in range(1, num_segments):
             x = v1.x + i * segment_length * dx
             y = v1.y + i * segment_length * dy
-            new_vertex = Vertex(x, y, self.next_new_index)
+            new_vertex = Vertex(x, y, self.next_new_index, index1=v1.index, index2=v2.index)
             self.next_new_index += 1
             new_vertices.append(new_vertex)
 
         return new_vertices
+
+    def _truncate_segment_new(self, v1, v2):
+        """Truncate segment if longer than max_length, return new intermediate vertices"""
+        distance = v1.distance_to(v2)
+        if distance <= self.max_length:
+            return None
+
+        num_segments = math.ceil(distance / self.max_length)
+        segment_length = distance / num_segments
+
+        dx = (v2.x - v1.x) / distance
+        dy = (v2.y - v1.y) / distance
+
+        x = v1.x + segment_length * dx
+        y = v1.y + segment_length * dy
+        new_vertex = Vertex(x, y, self.next_new_index, index1=v1.index, index2=v2.index)
+        self.next_new_index += 1
+
+        return new_vertex
 
     def process(self):
         original_vertices = self.original_polygon.vertices
@@ -71,31 +92,41 @@ class PolygonProcessor:
         b = original_vertices[split_point:][::-1]  # reversed
 
         # Process each pair
-        all_new_vertices = []
-        connection_map = []
-
-        for i in range(min(len(a), len(b))):
+        connection_map_new = {}
+        bNewPolygon = False
+        sPolyKeyNew = sPolyKey
+        v2_last = None
+        for i in range(min(len(a), len(b))):# v1在被沿着画画的边
             v1 = a[i]
             v2 = b[i]
-            print("PolygonProcessor process v1:", v1, " v2:", v2)
+            print("PolygonProcessor process v1:", v1, " v2:", v2.index1)
             # Get intermediate vertices for this segment
-            intermediate = self._truncate_segment(v1, v2)
+            intermediate = self._truncate_segment_new(v1, v2) # return one vertex
             print("PolygonProcessor intermediate:", intermediate)
 
-            all_new_vertices.extend(intermediate)
+            # if len(intermediate) != 0:
+            if intermediate is not None:
+                connection_map_new.setdefault(sPolyKey, []).extend([v1, intermediate])
+                if bNewPolygon == False: # 开启新的polygon分支
+                    bNewPolygon = True
+                    sPolyKeyNew = sPolyKeyNew + "_1"
+                    if v2_last != None:#把前一个点也加进来，让新老polygon虽然可能有重叠，但可以完全覆盖原来的图形
+                        connection_map_new.setdefault(sPolyKeyNew, []).append(v2_last)
 
-            # Store connection info
-            connection_map.append({
-                'from': v1,
-                'to': v2,
-                'intermediate': intermediate
-            })
+                connection_map_new.setdefault(sPolyKeyNew, []).extend([v2, intermediate])
+            else:
+                if bNewPolygon == True:# 新的polygon结束，回到老的polygon
+                    bNewPolygon = False
+                    connection_map_new.setdefault(sPolyKeyNew, []).append(v2)#多增加一个点，让新老polygon虽然可能有重叠，但可以完全覆盖原来的图形
+                connection_map_new.setdefault(sPolyKey, []).extend([v1, v2])
 
-        self.new_vertices = all_new_vertices
+            v2_last = v2
 
+        print("connection_map_new:", connection_map_new)
         # Generate new polygons
-        polygons = self._generate_polygons(connection_map)
-        return polygons
+        # polygons = self._generate_polygons(connection_map)
+        polygons_new = self._generate_polygons_new(connection_map_new)
+        return polygons_new
 
     def _generate_polygons(self, connection_map):
         polygons = []
@@ -112,6 +143,25 @@ class PolygonProcessor:
             ]
 
             polygons.append(Polygon(vertices))
+
+        return polygons
+
+    def _generate_polygons_new(self, connection_map):
+        polygons = []
+
+        for key, vertices in connection_map.items():
+            poly = []
+            for vertice in vertices:
+                print("_generate_polygons_new key:", key, " type:", type(vertice))
+                print("_generate_polygons_new:", vertice)
+                if key == sPolyKey and vertice.index2 != 0:
+                    vertice.index = vertice.index2
+                elif key != sPolyKey and vertice.index1 != 0:
+                    vertice.index = vertice.index1
+            sorted_vertices = sorted(vertices, key=lambda v: (v.index is not None, v.index))
+            print("_generate_polygons_new sorted_vertices:", sorted_vertices)
+            poly = Polygon(sorted_vertices)
+            polygons.append(poly)
 
         return polygons
 
@@ -163,17 +213,30 @@ class PolygonProcessor:
 
 
 # Example usage:
+# original_vertices = [
+#     Vertex(0, 0, 0),
+#     Vertex(2, 0, 1),
+#     Vertex(3, 1, 2),
+#     Vertex(2, 2, 3),
+#     Vertex(0, 2, 4),
+#     Vertex(-1, 1, 5)
+# ]
+
 original_vertices = [
     Vertex(0, 0, 0),
-    Vertex(2, 0, 1),
-    Vertex(3, 1, 2),
-    Vertex(2, 2, 3),
-    Vertex(0, 2, 4),
-    Vertex(-1, 1, 5)
+    Vertex(1, -0.2, 1),
+    Vertex(2, 0, 2),
+    Vertex(3, 1, 3),
+    Vertex(2, 2, 4),
+    Vertex(1, 2.3, 5),
+    Vertex(0, 2, 6),
+    Vertex(-0.5, 1.5, 7),
+    Vertex(-1, 1, 8)
 ]
 
 original_polygon = Polygon(original_vertices)
-processor = PolygonProcessor(original_polygon, max_length=0.8)
+# processor = PolygonProcessor(original_polygon, max_length=0.8)
+processor = PolygonProcessor(original_polygon, max_length=1.8)
 result_polygons = processor.process()
 
 print("Original polygon:", original_polygon)
